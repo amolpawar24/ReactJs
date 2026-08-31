f6b16fdb3bef327c7f7707004c5266725e40a509
amolpawar24 <amolpawar.me@gmail.com>
2026-08-31 17:00:52 +0530

    feat: add initial Component.js file with component structure and usage notes

---
+++ COMPONENTS/Component.js
@@ -0,0 +1,12 @@
+//! Compoenents 
+
+//todo:- Components are like functions that return HTML elements.
+//todo:- Component describe part of the User Interface.
+//todo:- They are reusable and can be nested inside other component.
+//todo:- A component is considered the core building blocks of a React Application.
+//todo:- It makes the tasks of building UI much easier.
+//todo:- Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML.
+//todo:- Each component exist in the same space but they work independently from the one another and merge all in parent component which will be the final UI of your application.
+//todo:- Every React component has its own structure , methods  and API
+//todo:- They can be reusable as per your need.
+//todo:- For Reusing component we need do import and export the component.
