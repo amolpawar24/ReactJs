44d235799c6cd15ada08ccb8aacc0014a62806e8
amolpawar24 <amolpawar.me@gmail.com>
2026-08-31 18:44:09 +0530

    feat: add MultipleArrowFunction component with multiple arrow functions

--- COMPONENTS/TYPES/FUNCTIONAL COMPONENTS/functional-components/src/App.jsx
+++ COMPONENTS/TYPES/FUNCTIONAL COMPONENTS/functional-components/src/App.jsx
@@ -1,10 +1,25 @@
+/* eslint-disable no-unused-vars */
 import Functional1 from "./components/Functional1"
-
+import Arrow from "./components/ArrowFunctional"
+import DefFunction, { Fun1, Fun2, Fun3, Fun4, Fun5 } from "./components/MultipleArrowFunction"
 function App() {
 
   return (
     <>
-      <Functional1/>
+      {/* Normal Function */}
+      {/* <Functional1/> */}
+
+      {/* Arrow Function  */}
+      {/* <Arrow/> */}
+
+      {/* Multiple Arrow Functions  */}
+      {/* <Fun1/> */}
+      {/* <Fun2/> */}
+      {/* <Fun3/> */}
+      {/* <Fun4/> */}
+      {/* <Fun5/> */}
+      {/* <DefFunction/> */}
+
     </>
   )
 }
---
+++ COMPONENTS/TYPES/FUNCTIONAL COMPONENTS/functional-components/src/components/MultipleArrowFunction.jsx
@@ -0,0 +1,48 @@
+export let Fun1 = () => {
+  return (
+    <>
+      <h1>Hello This is Function 1</h1>
+    </>
+  );
+};
+
+export let Fun2 = () => {
+  return (
+    <>
+      <h1>Hello This is Function 2</h1>
+    </>
+  );
+};
+
+export let Fun3 = () => {
+  return (
+    <>
+      <h1>Hello This is Function 3</h1>
+    </>
+  );
+};
+
+export let Fun4 = () => {
+  return (
+    <>
+      <h1>Hello This is Function 4</h1>
+    </>
+  );
+};
+
+export let Fun5 = () => {
+  return (
+    <>
+      <h1>Hello This is Function 5</h1>
+    </>
+  );
+};
+
+let DefFunction = () => {
+  return (
+    <div>
+      <h1>Hello This is Default Function</h1>
+    </div>
+  );
+};
+export default DefFunction;
