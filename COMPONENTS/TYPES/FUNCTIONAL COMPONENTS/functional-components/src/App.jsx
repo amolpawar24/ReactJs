90ada902e3bfa7eb0de176a8f34819dbd3235da8
amolpawar24 <amolpawar.me@gmail.com>
2026-08-31 18:24:46 +0530

    - Introduced Functional1 component to demonstrate a simple functional component.

---
+++ COMPONENTS/TYPES/FUNCTIONAL COMPONENTS/functional-components/src/App.jsx
@@ -0,0 +1,12 @@
+import Functional1 from "./components/Functional1"
+
+function App() {
+
+  return (
+    <>
+      <Functional1/>
+    </>
+  )
+}
+
+export default App
---
+++ COMPONENTS/TYPES/FUNCTIONAL COMPONENTS/functional-components/src/components/Functional1.jsx
@@ -0,0 +1,9 @@
+function Functional1() {
+  return (
+    <>
+      <h1> This is Functional Component 1</h1>
+    </>
+  );
+}
+
+export default Functional1;
