9401776e07eaa11628dc265a1b504521eb42b9d2
amolpawar24 <amolpawar.me@gmail.com>
2026-09-03 16:04:49 +0530

    feat: add Fragment2 component demonstrating usage of parent tags

---
+++ FRAGMENTS/fragments/src/components/Fragment2.jsx
@@ -0,0 +1,14 @@
+
+const Fragment2 = () =>{
+    //! First Way Using Parent Tags
+    //? Add One Parent Tag to Wrap All Elements 
+    return(
+        <div>
+            <h1>Element 1</h1>
+            <h1>Element 2</h1>
+            <h1>Element 3</h1>
+        </div>
+    )
+}
+
+export default Fragment2;
