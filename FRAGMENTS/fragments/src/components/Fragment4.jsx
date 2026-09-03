7e93a164e4bbb28a6b52d9a800017dc9163d2eeb
amolpawar24 <amolpawar.me@gmail.com>
2026-09-03 16:06:13 +0530

    feat: add Fragment4 component demonstrating usage of React.Fragment

---
+++ FRAGMENTS/fragments/src/components/Fragment4.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react"
+
+const Fragment4 = () =>{
+    //! 3rd Way Using Fragment 
+    //todo: Extract the Fragment From React and Use it Directly
+    return(
+        <Fragment>
+            <h1>Element 1</h1>
+            <h1>Element 2</h1>
+            <h1>Element 3</h1>
+        </Fragment>
+    )
+}
+
+export default Fragment4;
