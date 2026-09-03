f69580da26e40ce9d0f9fc2887149201c57bd0c9
amolpawar24 <amolpawar.me@gmail.com>
2026-09-03 16:03:47 +0530

    Created the Fragment to understand the Fragment in React

---
+++ FRAGMENTS/fragments/src/components/Fragment1.jsx
@@ -0,0 +1,12 @@
+
+const Fragment1 = () =>{
+    
+    //! We cannot return Two Components
+
+    // return(
+    //    <h1>This is Fragment1 Component</h1>
+    //    <h2>This is Fragment1 Component</h2>
+    // )
+}
+
+export default Fragment1;
