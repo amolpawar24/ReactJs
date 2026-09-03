f3145347d4ea49c3a505e1245ee4e4bc6100000d
amolpawar24 <amolpawar.me@gmail.com>
2026-09-03 16:05:20 +0530

    feat: add Fragment3 component demonstrating usage of React.Fragment

---
+++ FRAGMENTS/fragments/src/components/Fragment3.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Fragment3 = () =>{
+    //! 2nd Way Using <React.Fragment></React.Fragment>
+    //todo : For that you need to import the React From React 
+    return(
+        <React.Fragment>
+            <h1>Element 1</h1>
+            <h1>Element 2</h1>
+            <h1>Element 3</h1>
+        </React.Fragment>
+    )
+}
+
+export default Fragment3;
