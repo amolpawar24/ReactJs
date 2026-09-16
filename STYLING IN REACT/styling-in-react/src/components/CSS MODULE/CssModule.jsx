ecc38972286fae52af7b997cf2f6e361a4aa8256
amolpawar24 <amolpawar.me@gmail.com>
2026-09-16 15:42:12 +0530

    feat: add CssModule component with CSS module styling

---
+++ STYLING IN REACT/styling-in-react/src/components/CSS MODULE/CssModule.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import styles from './style.module.css'
+export default function CssModule() {
+  return (
+    <h1 className={styles.primary}>This Is CssModule</h1>
+  )
+}
