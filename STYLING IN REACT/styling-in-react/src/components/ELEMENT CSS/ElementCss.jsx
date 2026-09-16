2b2df45f5fa9ddb242d03882058db89df7e5406d
amolpawar24 <amolpawar.me@gmail.com>
2026-09-16 15:42:37 +0530

    feat: add ElementCss component with inline styling demonstration

---
+++ STYLING IN REACT/styling-in-react/src/components/ELEMENT CSS/ElementCss.jsx
@@ -0,0 +1,10 @@
+export default function ElementCss() {
+    const myStyle = {
+        color :"red",
+        fontSize : "35px",
+        backgroundColor : "yellow"
+    }
+  return (
+    <h3 style={myStyle}>This Is Element Css</h3>
+  )
+}
