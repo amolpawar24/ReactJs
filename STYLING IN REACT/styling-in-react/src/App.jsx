c91845c14edabdc4c118677f2c03d0c785ad354d
amolpawar24 <amolpawar.me@gmail.com>
2026-09-16 15:42:05 +0530

    Add initial project structure and components for styling in React
    - Created SVG icons for various applications (bluesky, discord, documentation, github, social, x).
    - Implemented main App component with routes for different CSS styling methods.
    - Added assets including hero image, react logo, and vite logo.
    - Introduced TypesCss.html to document CSS assignment methods.
    - Set up main entry point (main.jsx) for React application.
    - Configured Vite for project build and development.

---
+++ STYLING IN REACT/styling-in-react/src/App.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import CssModule from "./components/CSS MODULE/CssModule"
+import ExternalCss from "./components/CSS STYLESHEET [ EXTERNAL ]/ExternalCss"
+import ElementCss from "./components/ELEMENT CSS/ElementCss"
+import FrameworkCss from "./components/FRAMEWORK/FrameworkCss"
+import InlineCss from "./components/INLINE CSS/InlineCss"
+
+function App() {
+
+  return (
+    <>
+        {/* 1.CSS MODULE */}
+        {/* <CssModule/> */}
+
+        {/* 2.CSS STYLESHEET */}
+        {/* <ExternalCss/> */}
+
+        {/* 3.ElEMENT CSS */}
+        {/* <ElementCss/> */}
+
+        {/* 4.FRAMEWORK CSS */}
+        {/* <FrameworkCss/> */}
+
+        {/* 5.INLINE CSS */}
+        <InlineCss/>
+    </>
+  )
+}
+
+export default App
