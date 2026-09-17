ed470c869281e6b32ca22eeba674657c281b5e02
amolpawar24 <amolpawar.me@gmail.com>
2026-09-17 09:43:04 +0530

    feat: add ArrayProps component to display array values using props

---
+++ PROPS/props/src/components/Functional Components/ArrayProps.jsx
@@ -0,0 +1,10 @@
+
+export default function ArrayProps(props){
+    return(
+        <>
+            <h3>Array Props</h3>
+            <h5>Array Index value [0] is : {props.arr[0]}</h5>
+            <h5>Array Index value [1] is : {props.arr[1]}</h5>
+        </>
+    )
+}
